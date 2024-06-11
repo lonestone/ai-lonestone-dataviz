d3181925c72f43df0fda691a2f6620fd386b8ebe
Kevin Struillou <kevin.struillou@lonestone.io>
2024-06-11 16:48:52 +0200

    attempt to fix zod self ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import z from "zod";
+import z, { ZodDiscriminatedUnion, ZodTypeAny } from "zod";
 import fakeSchema from "./fakeSchema.json";
 
 // const Data = {
@@ -23,18 +23,13 @@ const columnJsonSchema = columnSchemaBase.extend({
 
 const columnNumberSchema = columnSchemaBase.extend({
   type: z.literal("number"),
-  data: z.number(),
   // precision: z.number(),
   unit: z.string().optional(),
 });
 
-// type SchemaType = z.infer<typeof columnSchemaSchema> & {
-//   data: SchemaType[];
-// };
-
 const columnSchemaSchema = columnSchemaBase.extend({
   type: z.literal("schema"),
-  // data: z.array(z.lazy(() => columnSchema)),
+  schema: z.array(z.lazy(getColumnSchema)),
 });
 
 const columnSchema = z.discriminatedUnion("type", [
@@ -44,16 +39,20 @@ const columnSchema = z.discriminatedUnion("type", [
   columnSchemaSchema,
 ]);
 
-const evaluationResultSchema = z.object({
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getColumnSchema(): ZodDiscriminatedUnion<"type", any> {
+  return columnSchema
+}
+
+const evaluationSchema = z.object({
   columns: z.array(columnSchema),
 });
-
-type ColumnType = z.infer<typeof columnSchema>["type"];
+type Evaluation = z.infer<typeof evaluationSchema>
 
 function App() {
-  console.log("FakeSchema:", fakeSchema);
-  const data = evaluationResultSchema.safeParse(fakeSchema);
-  if (!data.success) console.debug("dynamicShema:", data.error);
+  const data = evaluationSchema.safeParse(fakeSchema);
+  if (!data.success) console.debug(data.error);
+  else console.debug("dynamicShema:", data.data)
 
   return (
     <Button className="px-[100px]" variant="outline">
