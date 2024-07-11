3526684536dc6c0ac228b84e3f5ff2a2b4662805
Noé Grangier <noe@lonestone.io>
2024-07-11 17:03:55 +0200

    Added DataTable as lib component

---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+import DataTable from "./organisms/DataTable";
+
+export default DataTable;
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import * as packageJson from './package.json'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve('src', 'components/index.ts'),
+      name: 'dataviz',
+      formats: ['es', 'umd'],
+      fileName: (format) => `dataviz.${format}.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(packageJson.peerDependencies)],
     },
   },
 });
